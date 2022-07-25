597167820188ffd2b65293489ca350d60c261460
Davi Santos <davidossantosfreitas19@gmail.com>
2022-07-24 21:57:23 -0300

    feat: add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { SearchBar } from "./components/SearchBar";
 import { Home } from "./pages/Home";
 import "./styles/global.css";
 
 function App() {
   return (
     <>
-      <Home/>
+      <div className="flex flex-col items-center justify-center w-full h-full bg-black-100">
+        <div className="w-full max-w-[1170px] h-full mt-14">
+          <SearchBar/>
+          <Home/>
+        </div>
+      </div>
     </>
   )
 }
