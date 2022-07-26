bbb892277d52e44a236c3fdbbaaa4e2c96074981
Davi Santos <davidossantosfreitas19@gmail.com>
2022-07-26 15:26:51 -0300

    feat: change theme button location

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Moon } from "phosphor-react";
 import { SearchBar } from "./components/SearchBar";
 import { Home } from "./pages/Home";
 import "./styles/global.css";
@@ -5,11 +6,14 @@ import "./styles/global.css";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full h-full bg-black-100">
+      <div className="relative flex flex-col items-center justify-center w-full h-full bg-black-100">
         <div className="flex flex-col w-full max-w-[1170px] h-full mt-14 gap-y-8">
           <SearchBar/>
           <Home/>
         </div>
+        <button className="absolute top-5 right-5 w-12 h-12 flex items-center justify-center bg-black-200 bg-opacity-50 rounded-md shadow-md">
+            <Moon  weight="fill" size={25} color="#CDCDCD"/>
+        </button>
       </div>
     </>
   )
