1e65355b9a688a282fbd35a0b57b6de4fe421e03
Davi Santos <davidossantosfreitas19@gmail.com>
2022-07-25 21:43:37 -0300

    feat: fix gap-y's value

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full h-full bg-black-100">
-        <div className="w-full max-w-[1170px] h-full mt-14">
+        <div className="flex flex-col w-full max-w-[1170px] h-full mt-14 gap-y-8">
           <SearchBar/>
           <Home/>
         </div>
