af45ba21ac7c89a0676b046d88a9623e2bfe4ed3
Davi Santos <davidossantosfreitas19@gmail.com>
2022-07-26 15:26:18 -0300

    feat: make loading cards responsived

--- src/components/CardLoading.tsx
+++ src/components/CardLoading.tsx
@@ -1,6 +1,6 @@
 export function CardLoading() {
     return (
-        <div className="animate-pulse border border-purple-100 shadow rounded-md p-4 w-[170px] mx-auto">
+        <div className="animate-pulse border border-purple-100 shadow rounded-md p-4 w-full md:w-[170px] mx-auto">
             <div className="flex items-center space-x-4">
               <div className="rounded-full bg-purple-100 opacity-70 h-5 w-5"></div>
               <div className="flex-1 rounded-full bg-purple-100 opacity-70 h-2 w-10"></div>
--- src/components/CardProfileLoading.tsx
+++ src/components/CardProfileLoading.tsx
@@ -1,7 +1,7 @@
 export function CardProfileLoading() {
     const cardWidths = [20, 12, 20, 12, 20, 12]
     return (
-        <div className="relative col-start-1 col-end-4 row-start-1 row-end-3 animate-pulse border border-purple-100 shadow rounded-md p-4 w-[23.125rem] mx-auto">
+        <div className="relative w- col-start-1 col-end-13 xl:col-start-1 xl:col-end-4 xl:row-start-1 xl:row-end-3 animate-pulse border border-purple-100 shadow rounded-md p-4 w-full xl:w-[23.125rem] mx-auto">
             <div className="flex flex-col items-center justify-center gap-6">
                 <div className="rounded-full bg-purple-100 opacity-70 h-[9.375rem] w-[9.375rem]"></div>
                 <div className="rounded-full bg-purple-100 opacity-70 h-2 w-3/4"></div>
