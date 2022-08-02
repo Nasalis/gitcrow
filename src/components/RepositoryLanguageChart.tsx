6c72ca7ae0d7cdfb6875a29a5b7d38e354356d64
Davi Santos <davidossantosfreitas19@gmail.com>
2022-08-01 22:37:37 -0300

    feat: add initial languages chart

---
+++ src/components/RepositoryLanguageChart.tsx
@@ -0,0 +1,82 @@
+import { Pie, PieConfig } from "@ant-design/charts";
+import { calculatePercentualValue } from "../utils/calculatePercentualValue";
+import { darkThemeChart } from "../utils/darkThemeChartConfig";
+
+interface Props {
+  languages: {
+    node: {
+      name: string;
+      color: string;
+    }
+    size: number;
+  }[]
+  totalValue: number;
+}
+
+export function RepositoryLanguageChart({languages, totalValue}: Props) {
+    const data = [
+        {
+          type: 'Typescript',
+          value: 74.4,
+        },
+        {
+          type: 'Ruby',
+          value: 25.3,
+        },
+        {
+          type: 'Other',
+          value: 0.3,
+        },
+    ];
+
+    const data2 = languages?.map(language => {
+      return {
+        value: Number(calculatePercentualValue(language.size, totalValue).toFixed(2)), 
+        name: language.node.name, 
+        color: language.node.color 
+      }
+    })
+
+    const config: PieConfig = {
+        data: data2 || data,
+        theme: darkThemeChart,
+        appendPadding: 10,
+        angleField: 'value',
+        colorField: 'name',
+        color: languages?.map(language => language.node.color),  
+        radius: 1,
+        innerRadius: 0.6,
+        label: {
+        type: 'inner',
+        offset: '-50%',
+        content: '{value}',
+        style: {
+            textAlign: 'center',
+            fontSize: 14,
+            fontWeight: "bold"
+        },
+        },
+        interactions: [
+            {
+              type: 'element-selected',
+            },
+            {
+              type: 'element-active',
+            },
+        ],
+        statistic: {
+            title: false,
+            content: {
+              style: {
+                color: "#ffffff",
+                whiteSpace: 'pre-wrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+              },
+              content: 'Lang.\nPlot',
+            },
+        },
+    };
+    
+    return <Pie {...config} />;
+}
